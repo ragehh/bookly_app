7e42c2eaee2b9d76458118c5d3fb615485b46162
ragehh <mrageh68@gmail.com>
2025-09-11 20:01:46 +0300

    add Animation to Splash View

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,37 @@
 import 'package:bookly_app/core/utils/assets_data.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 4),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +41,18 @@ class SplashViewBody extends StatelessWidget {
       children: [
         Image.asset(AssetsData.logo),
         const SizedBox(height: 5),
-        const Text(
-          'Read free books',
-          style: TextStyle(fontSize: 16),
-          textAlign: TextAlign.center,
+        AnimatedBuilder(
+          animation: animationController,
+          builder: (BuildContext context, _) {
+            return SlideTransition(
+              position: slidingAnimation,
+              child: const Text(
+                'Read Free Books',
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+            );
+          },
         ),
       ],
     );
