df9019c90dcfcbb5f70f9b13e6116e992a848a5b
ragehh <mrageh68@gmail.com>
2025-09-18 16:01:24 +0300

    used cached network image

--- lib/Features/home/data/models/book_model/book_model.dart
+++ lib/Features/home/data/models/book_model/book_model.dart
@@ -27,33 +27,32 @@ class BookModel extends Equatable {
   });
 
   factory BookModel.fromJson(Map<String, dynamic> json) => BookModel(
-        kind: json['kind'] as String?,
-        id: json['id'] as String?,
-        etag: json['etag'] as String?,
-        selfLink: json['selfLink'] as String?,
-        volumeInfo:
-            VolumeInfo.fromJson(json['volumeInfo'] as Map<String, dynamic>),
-        saleInfo: json['saleInfo'] == null
-            ? null
-            : SaleInfo.fromJson(json['saleInfo'] as Map<String, dynamic>),
-        accessInfo: json['accessInfo'] == null
-            ? null
-            : AccessInfo.fromJson(json['accessInfo'] as Map<String, dynamic>),
-        searchInfo: json['searchInfo'] == null
-            ? null
-            : SearchInfo.fromJson(json['searchInfo'] as Map<String, dynamic>),
-      );
+    kind: json['kind'] as String?,
+    id: json['id'] as String?,
+    etag: json['etag'] as String?,
+    selfLink: json['selfLink'] as String?,
+    volumeInfo: VolumeInfo.fromJson(json['volumeInfo'] as Map<String, dynamic>),
+    saleInfo: json['saleInfo'] == null
+        ? null
+        : SaleInfo.fromJson(json['saleInfo'] as Map<String, dynamic>),
+    accessInfo: json['accessInfo'] == null
+        ? null
+        : AccessInfo.fromJson(json['accessInfo'] as Map<String, dynamic>),
+    searchInfo: json['searchInfo'] == null
+        ? null
+        : SearchInfo.fromJson(json['searchInfo'] as Map<String, dynamic>),
+  );
 
   Map<String, dynamic> toJson() => {
-        'kind': kind,
-        'id': id,
-        'etag': etag,
-        'selfLink': selfLink,
-        'volumeInfo': volumeInfo?.toJson(),
-        'saleInfo': saleInfo?.toJson(),
-        'accessInfo': accessInfo?.toJson(),
-        'searchInfo': searchInfo?.toJson(),
-      };
+    'kind': kind,
+    'id': id,
+    'etag': etag,
+    'selfLink': selfLink,
+    'volumeInfo': volumeInfo.toJson(),
+    'saleInfo': saleInfo?.toJson(),
+    'accessInfo': accessInfo?.toJson(),
+    'searchInfo': searchInfo?.toJson(),
+  };
 
   @override
   List<Object?> get props {
